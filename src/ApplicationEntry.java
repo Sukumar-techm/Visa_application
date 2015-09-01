427bc98634910b61fcdf2f07675d9fd939543102
sukumar <sk00374486@techmahindra.com>
2015-09-01 18:08:19 +0530

    Checking java file

--- src/ApplicationEntry.java
+++ src/ApplicationEntry.java
@@ -18,7 +18,7 @@ public class ApplicationEntry extends HttpServlet {
      */
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-
+        
     }
 
     /** Destroys the servlet.
